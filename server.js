209fadd114618dae09efdf93d90bc4db4f527839
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-06-25 11:44:45 -0700

    updated routes

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
