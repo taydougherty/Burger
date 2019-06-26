ab57c17a82bb27d0fbb34b55da17c63fff77002a
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-06-25 18:09:50 -0700

    updated controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,10 +2,8 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
@@ -17,25 +15,25 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
-    // Send back the ID of the new quote
+  burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update(
+  burger.update(
     {
-      sleepy: req.body.sleepy
+      devoured: "TRUE"
     },
     condition,
     function(result) {
       if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
+
         return res.status(404).end();
       }
       res.status(200).end();
@@ -44,5 +42,4 @@ router.put("/api/cats/:id", function(req, res) {
   );
 });
 
-// Export routes for server.js to use.
 module.exports = router;
